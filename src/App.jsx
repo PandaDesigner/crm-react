d1c48093fed3b0ca777ac01c8f4925031b12c58d
Pedro Fernandez <pand4designers@gmail.com>
2022-08-13 17:17:18 -0500

    subir info

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+import Inicio from './pages/Inicio';
+import NuevoCliente from './pages/NuevoCliente'
+import EdicionCliente from './pages/EdicionCliente'
+
+export default function App() {
+  return (
+<BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+  <Route path="/clientes" element={<Layout />}>
+    <Route index element={<Inicio />} />
+    <Route path="nuevo" element={<NuevoCliente />} />
+    <Route path="editar/:id" element={<EdicionCliente/>}/>
+  </Route>
+  </Routes>
+</BrowserRouter>
+  )
+}
+
